01786adc8d06d643f7915153b3ba2952e82a88fb
wakatsukiryousuke <mhf1994.rw@gmail.com>
2022-04-17 16:59:18 +0900

    first-commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,8 +21,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\FirstMiddleware::class
     ];
-
     /**
      * The application's route middleware groups.
      *
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'first' => \App\Http\Middleware\FirstMiddleware::class
     ];
 }
